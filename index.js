786393ef66d336efa3e0d77b447128865291ee98
Lela Deslauriers <lela.deslauriers@gmail.com>
2022-12-10 14:52:43 -0800

    commit tree 7d0a0421853cfcd2b7985ab4d08f14473ba4c383

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Your code here
+//1. Grab the target that we want to interact with
+const dodger = document.getElementById("dodger");
+
+//2. Define the callback function that will execute when the event is triggered
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+
+  //3. Connect the above to an event listener
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+
+  function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    if (left < 400 - 40) {
+        dodger.style.left = `${left + 1}px`;
+    }
+  }
+
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+  });
